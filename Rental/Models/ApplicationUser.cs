5b0715aa3a2bd862332c289cfe6cc342da949f1e
Newforce44 <allancook@crownmkg.com>
2020-01-24 11:05:20 -0500

    startup

---
+++ Rental/Models/ApplicationUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Rental.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+        [Required]
+        [Display(Name = "License Number")]
+        public string LicenseNumber { get; set; }
+    }
+}
